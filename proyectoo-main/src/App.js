a663dc569a2f88a16390149a4dba130e20dfe887
Santiago Peralta <89149095+MonsterPeralta@users.noreply.github.com>
2023-03-02 17:43:49 -0500

    Proyecto con dos componentes

---
+++ proyectoo-main/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='btn btn-primary'>TIENE BOOTSTRAP</button>
+    </div>
+  );
+}
+
+export default App;
